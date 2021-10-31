8f6b917046c04b177491e534176c1c3146058b1c
eko5624 <84926938+eko5624@users.noreply.github.com>
2021-10-31 21:07:40 +0800

    Update autoupdate.py

--- autoupdate.py
+++ autoupdate.py
@@ -30,7 +30,7 @@ for p in pkgs:
 pkgs['luajit-dev'] = x['LuaJIT']
 pkgs['vapoursynth-dev'] = x['VapourSynth'][1:]
 pkgs['ffmpeg-dev'] = x['ffmpeg']
-for t in ['batch-bleeding_edge-weekly.yml']:
+for t in ['build-weekly.yml']:
   with in_place.InPlace('.github/workflows/%s' % t, newline='') as f:
     for l in f:
       if (i:=l.find('key: mcf_')) > -1:
