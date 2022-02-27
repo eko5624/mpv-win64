60166c1564541aa5fbd4a8a1ca43a9372f1dad20
eko5624 <562433403@qq.com>
2022-02-27 15:50:10 +0800

    Update autoupdate.py

--- autoupdate.py
+++ autoupdate.py
@@ -7,7 +7,7 @@ x = json.loads(resp.read().decode('utf-8'))
 mingw = x['Mingw-w64'][:x['Mingw-w64'].find('ucrt')+4]
 with in_place.InPlace('.github/workflows/toolchain.yml', newline='') as f:
   for l in f:
-    if (i:=l.find('key: mcf_')) > -1:
+    if (i:=l.find('key: mcf_')) > 9999:
       l = '%s%s\n' % (l[:i+9], mingw)
     elif (i:=l.find('curl')) > -1:
       l = '%s%s.7z\n' % (l[:i+55], x['Mingw-w64'])
