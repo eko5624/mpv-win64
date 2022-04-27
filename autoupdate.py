8f17d2e4c87c727b14ada979de99e71cee4f8b42
eko5624 <562433403@qq.com>
2022-04-27 20:57:33 +0800

    Update autoupdate.py

--- autoupdate.py
+++ autoupdate.py
@@ -8,7 +8,7 @@ mingw = x['Mingw-w64'][:x['Mingw-w64'].find('ucrt')+4]
 with in_place.InPlace('.github/workflows/toolchain.yml', newline='') as f:
   for l in f:
     if (i:=l.find('key: mcf_')) > -1:
-      l = '%s%s${{ secrets.MCF }}\n' % (l[:i+9], mingw)
+      l = '%s%s-${{ secrets.MCF }}\n' % (l[:i+9], mingw)
     elif (i:=l.find('curl')) > -1:
       l = '%s%s.7z\n' % (l[:i+55], x['Mingw-w64'])
     f.write(l)
@@ -46,7 +46,7 @@ for t in ['mpv.yml', 'build-weekly.yml', 'libplacebo.yml', 'package.yml']:
   with in_place.InPlace('.github/workflows/%s' % t, newline='') as f:
     for l in f:
       if (i:=l.find('key: mcf_')) > -1:
-        l = '%s%s${{ secrets.MCF }}\n' % (l[:i+9], mingw)
+        l = '%s%s-${{ secrets.MCF }}\n' % (l[:i+9], mingw)
       elif (i:=l.find('/dev/')) > -1:
         r = l.find('-1-x86_64')
         rr = l.rfind('-', i, r)
