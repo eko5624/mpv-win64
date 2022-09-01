35eb9c9a8fddadf8fe7020f83cae04654f9c128f
eko5624 <562433403@qq.com>
2022-09-01 13:51:26 +0800

    Update autoupdate.py

--- autoupdate.py
+++ autoupdate.py
@@ -20,7 +20,16 @@ for p in pkgs:
     for l in f:
       if l.startswith('pkgver'):
         l = 'pkgver=%s\n' % pkgs[p]
-      f.write(l)         
+      f.write(l)    
+pkgs = {}          
+for p in ['mpv']:
+  pkgs['%s' % p] = x[p]
+for p in pkgs:
+  with in_place.InPlace('%s/PKGBUILD-new' % p, newline='') as f:
+    for l in f:
+      if l.startswith('pkgver'):
+        l = 'pkgver=%s\n' % pkgs[p]
+      f.write(l)       
 pkgs['mcfgthread'] = mingw[:8]
 pkgs['libvorbis_aotuv-dev'] = x['libvorbis']
 pkgs['luajit'] = x['LuaJIT']
@@ -30,13 +39,7 @@ pkgs['ffmpeg'] = x['ffmpeg']
 pkgs['mpv'] = x['mpv'] 
 pkgs['mujs'] = x['mujs']
 pkgs['rubberband'] = x['rubberband']
-pkgs['libsixel'] = x['libsixel']
-for p in pkgs:
-  with in_place.InPlace('%s/PKGBUILD-new' % p, newline='') as f:
-    for l in f:
-      if l.startswith('pkgver'):
-        l = 'pkgver=%s\n' % pkgs[p]
-      f.write(l) 
+pkgs['libsixel'] = x['libsixel'] 
 for p in [
   'amf',
   'angle',
