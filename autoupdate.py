413294f47d214ce0636954ddba6eb256f0957ef6
eko5624 <562433403@qq.com>
2022-09-02 00:20:10 +0800

    Update autoupdate.py

--- autoupdate.py
+++ autoupdate.py
@@ -4,6 +4,7 @@ import in_place
 
 resp = request.urlopen('https://github.com/eko5624/nginx-nosni/raw/master/old.json')
 x = json.loads(resp.read().decode('utf-8'))
+
 mingw = x['Mingw-w64'][:x['Mingw-w64'].find('ucrt')+4]
 with in_place.InPlace('.github/workflows/toolchain.yml', newline='') as f:
   for l in f:
@@ -12,8 +13,8 @@ with in_place.InPlace('.github/workflows/toolchain.yml', newline='') as f:
     elif (i:=l.find('curl')) > -1:
       l = '%s%s.7z\n' % (l[:i+55], x['Mingw-w64'])
     f.write(l)
-
-pkgs = {}          
+    
+pkgs = {} 
 pkgs['mpv'] = x['mpv']
 for p in ['freetype2', 'fribidi', 'harfbuzz', 'libjxl', 'vulkan']:
   pkgs['%s-dev' % p] = x[p]
@@ -22,13 +23,12 @@ for p in pkgs:
     for l in f:
       if l.startswith('pkgver'):
         l = 'pkgver=%s\n' % pkgs[p]
-      f.write(l)  
-      
+      f.write(l)        
 pkgs['mcfgthread'] = mingw[:8]
 pkgs['libvorbis_aotuv-dev'] = x['libvorbis']
 pkgs['luajit'] = x['LuaJIT'] 
 for p in ['ffmpeg', 'libsixel', 'luajit2', 'mujs', 'rubberband']
-  pkgs['%s' % p] = x[p]
+  pkgs['%s' % p] = x[p]  
 for p in [
   'amf',
   'angle',
@@ -85,6 +85,7 @@ pkgs['ffmpeg-dev'] = x['ffmpeg']
 pkgs['ffmpeg-git'] = x['ffmpeg']
 pkgs['libmpv-git'] = x['mpv']
 pkgs['mpv-git'] = x['mpv']
+
 for t in ['ffmpeg.yml', 'mpv-meson.yml', 'mpv-waf.yml', 'build-weekly.yml', 'package.yml']:
   with in_place.InPlace('.github/workflows/%s' % t, newline='') as f:
     for l in f:
