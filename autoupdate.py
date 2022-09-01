534517a38b74e8966fdbbcaa59ea11d62356828e
eko5624 <562433403@qq.com>
2022-09-01 23:18:38 +0800

    Update autoupdate.py

--- autoupdate.py
+++ autoupdate.py
@@ -13,22 +13,15 @@ with in_place.InPlace('.github/workflows/toolchain.yml', newline='') as f:
       l = '%s%s.7z\n' % (l[:i+55], x['Mingw-w64'])
     f.write(l)
 pkgs = {}          
-for p in ['freetype2', 'fribidi', 'harfbuzz', 'vulkan', 'libjxl']:
+pkgs['mpv'] = x['mpv']
+for p in ['freetype2', 'fribidi', 'harfbuzz', 'libjxl', 'vulkan']:
   pkgs['%s-dev' % p] = x[p]
 for p in pkgs:
   with in_place.InPlace('%s/PKGBUILD-new' % p, newline='') as f:
     for l in f:
       if l.startswith('pkgver'):
         l = 'pkgver=%s\n' % pkgs[p]
-      f.write(l)    
-# pkgs = {}          
-pkgs['mpv'] = x['mpv'] 
-for p in pkgs:
-  with in_place.InPlace('%s/PKGBUILD-new' % p, newline='') as f:
-    for l in f:
-      if l.startswith('pkgver'):
-        l = 'pkgver=%s\n' % pkgs[p]
-      f.write(l)       
+      f.write(l)         
 pkgs['mcfgthread'] = mingw[:8]
 pkgs['libvorbis_aotuv-dev'] = x['libvorbis']
 pkgs['luajit'] = x['LuaJIT']
@@ -45,9 +38,6 @@ for p in [
   'brotli', 
   'dav1d', 
   'ffnvcodec', 
-  'freetype2', 
-  'fribidi', 
-  'harfbuzz', 
   'highway', 
   'lame', 
   'lcms2', 
@@ -61,7 +51,6 @@ for p in [
   'libiconv',
   'libudfread', 
   'libjpeg', 
-  'libjxl', 
   'libmfx', 
   'libmodplug', 
   'libmysofa', 
@@ -82,7 +71,6 @@ for p in [
   'opus', 
   'shaderc', 
   'spirv-cross', 
-  'vulkan', 
   'zlib',
   ]:
   pkgs['%s-dev' % p] = x[p]
