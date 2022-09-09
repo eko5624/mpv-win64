7ecd7ad05b018aac2b1ebaf1353bc42a56af3547
eko5624 <562433403@qq.com>
2022-09-09 22:20:06 +0800

    Update autoupdate.py

--- autoupdate.py
+++ autoupdate.py
@@ -15,7 +15,7 @@ with in_place.InPlace('.github/workflows/toolchain.yml', newline='') as f:
     f.write(l)
     
 pkgs = {} 
-pkgs['libsixel'] = x['libsixel']
+pkgs['libsixel-dev'] = x['libsixel']
 pkgs['mpv'] = x['mpv']
 for p in ['freetype2', 'fribidi', 'harfbuzz', 'libjxl', 'vulkan']:
   pkgs['%s-dev' % p] = x[p]
@@ -27,9 +27,6 @@ for p in pkgs:
       f.write(l)        
 pkgs['mcfgthread'] = mingw[:8]
 pkgs['libvorbis_aotuv-dev'] = x['libvorbis']
-pkgs['luajit'] = x['LuaJIT'] 
-for p in ['ffmpeg', 'luajit2', 'mujs', 'rubberband']:
-  pkgs['%s' % p] = x[p]  
 for p in [
   'amf',
   'angle',
@@ -66,6 +63,8 @@ for p in [
   'libxvid', 
   'libzimg', 
   'mbedtls', 
+  'mujs',
+  'rubberband',
   'openal-soft', 
   'opus', 
   'shaderc', 
@@ -79,11 +78,8 @@ for p in pkgs:
       if l.startswith('pkgver'):
         l = 'pkgver=%s\n' % pkgs[p]
       f.write(l)
-pkgs['libsixel-dev'] = x['libsixel']
 pkgs['luajit-dev'] = x['LuaJIT']      
 pkgs['luajit2-dev'] = x['luajit2']
-pkgs['mujs-dev'] = x['mujs']
-pkgs['rubberband-dev'] = x['rubberband']
 pkgs['vapoursynth-dev'] = x['VapourSynth'][1:]
 pkgs['ffmpeg-dev'] = x['ffmpeg']
 pkgs['ffmpeg-git'] = x['ffmpeg']
