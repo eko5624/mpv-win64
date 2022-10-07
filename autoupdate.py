0887eb37ff972f38f8cd208372217adf2d1f86ee
eko5624 <562433403@qq.com>
2022-10-07 13:28:44 +0800

    Update autoupdate.py

--- autoupdate.py
+++ autoupdate.py
@@ -88,6 +88,7 @@ pkgs['ffmpeg-dev'] = x['ffmpeg']
 pkgs['ffmpeg-git'] = x['ffmpeg']
 pkgs['libmpv-git'] = x['mpv']
 pkgs['mpv-git'] = x['mpv']
+pkgs['yt-dlp'] = x['yt-dlp']
 
 for t in ['ffmpeg.yml', 'libplacebo.yml', 'shaderc.yml', 'mpv-meson.yml', 'mpv-waf.yml', 'build-weekly.yml', 'package.yml']:
   with in_place.InPlace('.github/workflows/%s' % t, newline='') as f:
