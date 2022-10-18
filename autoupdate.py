40e39ace8ff8de7ee70bd3e486b92a6879745716
eko5624 <562433403@qq.com>
2022-10-18 15:36:28 +0800

    Update autoupdate.py

--- autoupdate.py
+++ autoupdate.py
@@ -80,13 +80,14 @@ for p in pkgs:
       f.write(l)
 pkgs['libsixel-dev'] = x['libsixel']
 pkgs['luajit-dev'] = x['LuaJIT']      
-pkgs['luajit2-dev'] = x['luajit2']
 pkgs['mujs-dev'] = x['mujs']
 pkgs['rubberband-dev'] = x['rubberband']
 pkgs['vapoursynth-shared-dev'] = x['VapourSynth'][1:]
 pkgs['vapoursynth-shared'] = x['VapourSynth'][1:]
 pkgs['ffmpeg-shared-dev'] = x['ffmpeg']
 pkgs['ffmpeg-shared'] = x['ffmpeg']
+pkgs['luajit2-shared-dev'] = x['luajit2']
+pkgs['luajit2-shared'] = x['luajit2']
 pkgs['mpv-shared'] = x['mpv']
 pkgs['vulkan-shared-dev'] = x['vulkan']
 
