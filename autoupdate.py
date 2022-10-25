bcd6c8e6cd51ef65f2018808c34a931488001f70
eko5624 <562433403@qq.com>
2022-10-25 08:22:52 +0800

    Update autoupdate.py

--- autoupdate.py
+++ autoupdate.py
@@ -71,6 +71,7 @@ for p in [
   'shaderc',
   'spirv-cross',
   'uavs3d',
+  'vulkan',
   'zlib',
   ]:
   pkgs['%s-dev' % p] = x[p]
@@ -93,7 +94,7 @@ pkgs['luajit2-shared'] = x['luajit2']
 pkgs['mpv-shared'] = x['mpv']
 pkgs['vulkan-shared-dev'] = x['vulkan']
 
-for t in ['ffmpeg.yml', 'libplacebo.yml', 'shaderc.yml', 'mpv-meson.yml', 'mpv-waf.yml', 'build-weekly.yml', 'package.yml']:
+for t in ['ffmpeg.yml', 'libplacebo.yml', 'shaderc.yml', 'vulkan', 'mpv-meson.yml', 'mpv-waf.yml', 'build-weekly.yml', 'package.yml']:
   with in_place.InPlace('.github/workflows/%s' % t, newline='') as f:
     for l in f:
       if (i:=l.find('key: mcf_')) > -1:
