4c8f1e62737780a8b38cc94d2ec65993f802ded4
eko5624 <562433403@qq.com>
2022-11-07 00:32:33 +0800

    Update release-dev.sh

--- release-dev.sh
+++ release-dev.sh
@@ -3,17 +3,15 @@
 GH_TOKEN=(${{ secrets.BOT }})
 CURL_RETRIES=("--connect-timeout 60 --retry 999 --retry-delay 5")
 
-
 # Delete assets
 asset_id=$(curl -u $GITHUB_ACTOR:"${GH_TOKEN[@]}" "${CURL_RETRIES[@]}" \
   -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/repos/${{ github.repository }}/releases/tags/dev \
-  | jq -r '.assets[] | select(.name | startswith("vulkan-dev")) | .id')                        
+  | jq -r '.assets[] | select(.name | startswith("$1")) | .id')                        
 curl -u $GITHUB_ACTOR:${GH_TOKEN[@]}" $CURL_RETRIES \
   -X DELETE \
   -H "Accept: application/vnd.github.v3+json" \
-  https://api.github.com/repos/${{ github.repository }}/releases/assets/$asset_id
-    
+  https://api.github.com/repos/${{ github.repository }}/releases/assets/$asset_id    
 
 # Release assets
 curl -u $GITHUB_ACTOR:"${GH_TOKEN[@]}" "${CURL_RETRIES[@]}" \
@@ -24,7 +22,7 @@ curl -u $GITHUB_ACTOR:"${GH_TOKEN[@]}" "${CURL_RETRIES[@]}" \
   release_id=$(curl -u $GITHUB_ACTOR:"${GH_TOKEN[@]}" "${CURL_RETRIES[@]}" \
     -H "Accept: application/vnd.github.v3+json" \
     https://api.github.com/repos/${{ github.repository }}/releases/tags/dev | jq -r '.id')
-  for f in vulkan-dev/*.zst; do 
+  for f in $2/*.zst; do 
     curl -u $GITHUB_ACTOR:"${GH_TOKEN[@]}" "${CURL_RETRIES[@]}" \
       -X POST -H "Accept: application/vnd.github.v3+json" \
       -H "Content-Type: $(file -b --mime-type $f)" \
