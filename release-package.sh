490960a3b86f14f27ee9ca85df0240ecc1a292b2
eko5624 <562433403@qq.com>
2022-11-08 22:48:35 +0800

    Update release-package.sh

--- release-package.sh
+++ release-package.sh
@@ -2,14 +2,14 @@
 set -x  
 
 # Release assets
-curl -O https://github.com/eko5624/nginx-nosni/raw/master/old.json
+curl -OL https://github.com/eko5624/nginx-nosni/raw/master/old.json
 date=$(date +%Y-%m-%d)
-short_sha=$(cat old.json | jq -r '.mpv-git')
+short_sha=$(cat old.json | jq -r '."mpv-git"')
 curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
   -X POST \
   -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/repos/${GITHUB_REPOSITORY}/releases \
-  -d '{"tag_name": "$date","name":"$SHORT_DATE","body":"Bump to mpv-player/mpv@$short_sha"}'
+  -d '{"tag_name": "$date","name": "$date","body": "Bump to mpv-player/mpv@$short_sha"}'
   
 release_id=$(curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
   -H "Accept: application/vnd.github.v3+json" \
