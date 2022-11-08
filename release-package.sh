ea29f56fbe860a4c255d23f5414aa95d7e3c2eba
eko5624 <562433403@qq.com>
2022-11-08 23:03:13 +0800

    Update release-package.sh

--- release-package.sh
+++ release-package.sh
@@ -5,15 +5,19 @@ set -x
 curl -OL https://github.com/eko5624/nginx-nosni/raw/master/old.json
 date=$(date +%Y-%m-%d)
 short_sha=$(cat old.json | jq -r '."mpv-git"')
-curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
+release_id=$(curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
   -X POST \
   -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/repos/${GITHUB_REPOSITORY}/releases \
-  -d '{"tag_name": "$date","name": "$date","body": "Bump to mpv-player/mpv@$short_sha"}'
-  
-release_id=$(curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
-  -H "Accept: application/vnd.github.v3+json" \
-  https://api.github.com/repos/${GITHUB_REPOSITORY}/releases/tags/$SHORT_DATE | jq -r '.id')
+  -d @- <<END \
+  | jq -r '.id'
+{ 
+  "tag_name": "$date",
+  "name":"$date",
+  "body": "Bump to mpv-player/mpv@$short_sha"
+}
+END
+)
   
 for f in git*.7z; do
   curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
