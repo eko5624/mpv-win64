4b79548616d40a72480cb4c046510edb92b65341
eko5624 <562433403@qq.com>
2022-11-09 11:28:38 +0800

    Update release-package.sh

--- release-package.sh
+++ release-package.sh
@@ -3,7 +3,7 @@ set -x
 
 # Release assets
 curl -OL https://github.com/eko5624/nginx-nosni/raw/master/old.json
-short_sha=$(cat old.json | jq -r '."mpv-git"' | head -c 7)
+short_sha=$(cat old.json | jq -r '."mpv"')
 date=$(date +%Y-%m-%d)
 
 curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
