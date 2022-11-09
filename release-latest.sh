a822e2a8d91610d69db1ab7fc7f627eb81ef84a0
eko5624 <562433403@qq.com>
2022-11-09 17:58:37 +0800

    Create release-latest.sh

---
+++ release-latest.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -x
+
+# Delete assets
+asset_id=$(curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
+  -H "Accept: application/vnd.github.v3+json" \
+  https://api.github.com/repos/${GITHUB_REPOSITORY}/releases/tags/latest \
+  | jq -r '.assets[] | select(.name | startswith("'"$1"'")) | .id')
+
+for f in $asset_id; do
+  curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
+    -X DELETE \
+    -H "Accept: application/vnd.github.v3+json" \
+    https://api.github.com/repos/${GITHUB_REPOSITORY}/releases/assets/$asset_id;
+done    
+
+# Release assets
+curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
+  -X POST \
+  -H "Accept: application/vnd.github.v3+json" \
+  https://api.github.com/repos/${GITHUB_REPOSITORY}/releases \
+  -d '{"tag_name": "latest"}'
+  
+release_id=$(curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
+  -H "Accept: application/vnd.github.v3+json" \
+  https://api.github.com/repos/${GITHUB_REPOSITORY}/releases/tags/latest | jq -r '.id')
+  
+for f in $2/*.xz; do 
+  curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
+    -X POST -H "Accept: application/vnd.github.v3+json" \
+    -H "Content-Type: $(file -b --mime-type $f)" \
+    https://uploads.github.com/repos/${GITHUB_REPOSITORY}/releases/$release_id/assets?name=$(basename $f) \
+    --data-binary @$f; 
+done
