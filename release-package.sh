fe08579dbafe8d8caeee2bce28ee5f933bf8c907
eko5624 <562433403@qq.com>
2022-11-09 18:02:02 +0800

    Create release-package.sh

---
+++ release-package.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -x  
+
+# Release assets
+date=$(date +%Y-%m-%d)
+  
+release_id=$(curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
+  -H "Accept: application/vnd.github.v3+json" \
+  https://api.github.com/repos/${GITHUB_REPOSITORY}/releases/tags/$date | jq -r '.id')
+  
+for f in *.7z; do
+  curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
+    -X POST -H "Accept: application/vnd.github.v3+json" \
+    -H "Content-Type: $(file -b --mime-type $f)" \
+    https://uploads.github.com/repos/${GITHUB_REPOSITORY}/releases/$release_id/assets?name=$(basename $f) --data-binary @$f; 
+done
