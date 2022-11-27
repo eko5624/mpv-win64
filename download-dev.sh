9a8d9376665e00af5f86b26651704395397171ff
eko5624 <562433403@qq.com>
2022-11-27 15:13:01 +0800

    Update download-dev.sh

--- download-dev.sh
+++ download-dev.sh
@@ -4,7 +4,7 @@ set -x
 download_url=($(curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
   -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/repos/${GITHUB_REPOSITORY}/releases/tags/dev \
-  | $jq -r '.assets[] | select(.name | startswith("'"$1"'")) | .browser_download_url' | tr -d '\r'))
+  | jq -r '.assets[] | select(.name | startswith("'"$1"'")) | .browser_download_url' | tr -d '\r'))
 
 for url in "${download_url[@]}"; do
   curl -OL $url;
