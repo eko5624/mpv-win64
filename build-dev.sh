fe401d7a3826d64cfc529d2ac6285667a0ed7e15
eko5624 <562433403@qq.com>
2022-11-27 15:51:32 +0800

    Update and rename download-dev.sh to build-dev.sh

--- build-dev.sh
+++ build-dev.sh
@@ -9,5 +9,7 @@ download_url=($(curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
 for url in "${download_url[@]}"; do
   curl -OL $url;
 done
+pacman -U *.zst --noconfirm
+cd "$2"; PATH=/usr/bin:/opt/bin:/d/ucrt64/bin PKG_CONFIG_PATH=/opt/lib/pkgconfig makepkg
   
   
