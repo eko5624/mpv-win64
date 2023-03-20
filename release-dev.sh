124b646b07c19d1c886a603f77f5195d7f07bbfd
eko5624 <562433403@qq.com>
2023-03-20 22:04:41 +0800

    Update release-dev.sh

--- release-dev.sh
+++ release-dev.sh
@@ -3,29 +3,29 @@ set -x
 
 # Delete assets
 asset_id=$(curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
-  -H "Accept: application/vnd.github.v3+json" \
+  -H "Accept: application/vnd.github+json" \
   https://api.github.com/repos/${GITHUB_REPOSITORY}/releases/tags/dev \
   | jq -r '.assets[] | select(.name | startswith("'"$1"'")) | .id') 
   
 curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
   -X DELETE \
-  -H "Accept: application/vnd.github.v3+json" \
+  -H "Accept: application/vnd.github+json" \
   https://api.github.com/repos/${GITHUB_REPOSITORY}/releases/assets/$asset_id    
 
 # Release assets
 curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
   -X POST \
-  -H "Accept: application/vnd.github.v3+json" \
+  -H "Accept: application/vnd.github+json" \
   https://api.github.com/repos/${GITHUB_REPOSITORY}/releases \
   -d '{"tag_name": "dev"}'
   
 release_id=$(curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
-  -H "Accept: application/vnd.github.v3+json" \
+  -H "Accept: application/vnd.github+json" \
   https://api.github.com/repos/${GITHUB_REPOSITORY}/releases/tags/dev | jq -r '.id')
   
 for f in $2/*.zst; do 
   curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
-    -X POST -H "Accept: application/vnd.github.v3+json" \
+    -X POST -H "Accept: application/vnd.github+json" \
     -H "Content-Type: $(file -b --mime-type $f)" \
     https://uploads.github.com/repos/${GITHUB_REPOSITORY}/releases/$release_id/assets?name=$(basename $f) --data-binary @$f; 
 done
