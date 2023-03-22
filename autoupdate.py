cf6be8dc364ca99cc66220d0f46772393097c356
eko5624 <562433403@qq.com>
2023-03-22 10:39:52 +0800

    Update autoupdate.py

--- autoupdate.py
+++ autoupdate.py
@@ -90,13 +90,12 @@ pkgs['luajit2-dev'] = x['luajit2']
 pkgs['mujs-dev'] = x['mujs']
 pkgs['rubberband-dev'] = x['rubberband']
 pkgs['vapoursynth-dev'] = x['VapourSynth'][1:]
-pkgs['curl-dev'] = x['curl']
 pkgs['ffmpeg-dev'] = x['ffmpeg']
 pkgs['ffmpeg-git'] = x['ffmpeg']
 pkgs['libmpv-git'] = x['mpv']
 pkgs['mpv-git'] = x['mpv']
 
-for t in ['ffmpeg.yml', 'libplacebo.yml', 'shaderc.yml', 'vulkan.yml', 'mpv-meson.yml', 'build-weekly.yml', 'package.yml', 'toolchain.yml']:
+for t in ['curl.yml', 'ffmpeg.yml', 'libplacebo.yml', 'shaderc.yml', 'vulkan.yml', 'mpv-meson.yml', 'build-weekly.yml', 'package.yml']:
   with in_place.InPlace('.github/workflows/%s' % t, newline='') as f:
     for l in f:
       if (i:=l.find('key: mcf_')) > -1:
