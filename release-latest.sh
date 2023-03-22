9bfc08ae90dae727a4b3cb6faafe593e4bc33b8b
eko5624 <562433403@qq.com>
2023-03-23 07:46:23 +0800

    Update release-latest.sh

--- release-latest.sh
+++ release-latest.sh
@@ -2,34 +2,34 @@
 set -x
 DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 export PATH="$DIR/bin:$PATH"
-CURL=$DIR/bin/curl
+#CURL=$DIR/bin/curl
 
 # Delete assets
-asset_id=($($CURL -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
+asset_id=($($DIR/bin/curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
   -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/repos/${GITHUB_REPOSITORY}/releases/tags/latest \
 | jq -r '.assets[] | select(.name | startswith("'"$1"'")) | .id' | tr -d '\r'))
 
 for id in "${asset_id[@]}"; do
-  $CURL -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
+  $DIR/bin/curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
     -X DELETE \
     -H "Accept: application/vnd.github.v3+json" \
     https://api.github.com/repos/${GITHUB_REPOSITORY}/releases/assets/$id;
 done    
 
 # Release assets
-$CURL -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
+$DIR/bin/curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
   -X POST \
   -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/repos/${GITHUB_REPOSITORY}/releases \
   -d '{"tag_name": "latest"}'
   
-release_id=$($CURL -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
+release_id=$($DIR/bin/curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
   -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/repos/${GITHUB_REPOSITORY}/releases/tags/latest | jq -r '.id')
   
 for f in $2/*.xz; do 
-  $CURL -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
+  $DIR/bin/curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
     -X POST -H "Accept: application/vnd.github.v3+json" \
     -H "Content-Type: $(file -b --mime-type $f)" \
     https://uploads.github.com/repos/${GITHUB_REPOSITORY}/releases/$release_id/assets?name=$(basename $f) \
