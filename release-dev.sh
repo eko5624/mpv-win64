94b21737d77cdfed871ecf4ee0e721d135bcd5ec
eko5624 <562433403@qq.com>
2023-03-23 09:14:56 +0800

    Update release-dev.sh

--- release-dev.sh
+++ release-dev.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 set -x
-
-#DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-#export PATH="$DIR/bin:$PATH"
 CURL=/d/ucrt64/bin/curl
 
 # Delete assets
@@ -29,7 +26,7 @@ release_id=$($CURL -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
   
 for f in $2/*.zst; do 
   $CURL -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
-    -X POST -H "Accept: application/vnd.github+json" \
+    -X POST -H "Accept: application/vnd.github.v3+json" \
     -H "Content-Type: $(file -b --mime-type $f)" \
     https://uploads.github.com/repos/${GITHUB_REPOSITORY}/releases/$release_id/assets?name=$(basename $f) --data-binary @$f; 
 done
