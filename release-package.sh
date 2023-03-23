b40d0d03cd577c74afe023e23c4aa41ef6f568a6
eko5624 <562433403@qq.com>
2023-03-23 15:49:16 +0800

    Update release-package.sh

--- release-package.sh
+++ release-package.sh
@@ -9,7 +9,7 @@ release_id=$($CURL -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
   https://api.github.com/repos/${GITHUB_REPOSITORY}/releases/tags/$date | jq -r '.id')
   
 for f in *.7z; do
-  curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
+  $CURL -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
     -X POST -H "Accept: application/vnd.github.v3+json" \
     -H "Content-Type: $(file -b --mime-type $f)" \
     https://uploads.github.com/repos/${GITHUB_REPOSITORY}/releases/$release_id/assets?name=$(basename $f) --data-binary @$f; 
