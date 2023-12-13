2b3f0d7f079b65223996329d0934cddd4fe3e916
eko5624 <562433403@qq.com>
2023-12-13 17:00:37 +0800

    Update release-package.sh

--- release-package.sh
+++ release-package.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
-set -x  
-CURL=/d/ucrt64/bin/curl
-CURL_RETRIES="--connect-timeout 60 --retry 5 --retry-delay 5 --http1.1"
+set -x
+CURL_RETRIES="--connect-timeout 60 --retry 5 --retry-delay 5"
 
 #Get mpv latest commit sha
 short_sha=$(cat /d/msys64/opt/mpv/SHORT_SHA)
-
-# Release assets
 date=$(date +%Y-%m-%d)
 
-$CURL -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
+
+#Release note
+body="Bump to mpv-player/mpv@${short_sha}\n"
+body+="**Compiler**: clang"
+
+
+curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
   -X POST \
   -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/repos/${GITHUB_REPOSITORY}/releases \
   -d '{"tag_name":"'"$date"'","name":"'"$date"'","body":"Bump to mpv-player/mpv@'"$short_sha"'"}'
   
-release_id=$($CURL -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
+release_id=$(curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
   -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/repos/${GITHUB_REPOSITORY}/releases/tags/$date | jq -r '.id')
   
 for f in *.7z; do
-  $CURL -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
+  curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
     -X POST -H "Accept: application/vnd.github.v3+json" \
     -H "Content-Type: $(file -b --mime-type $f)" \
     https://uploads.github.com/repos/${GITHUB_REPOSITORY}/releases/$release_id/assets?name=$(basename $f) --data-binary @$f; 
