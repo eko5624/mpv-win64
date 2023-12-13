17d6fee82138f78807c1a761f4e5f1aabad4b319
eko5624 <562433403@qq.com>
2023-12-13 17:09:04 +0800

    Update release-package.sh

--- release-package.sh
+++ release-package.sh
@@ -14,7 +14,7 @@ curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
   -X POST \
   -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/repos/${GITHUB_REPOSITORY}/releases \
-  -d '{"tag_name":"'"$date"'","name":"'"$date"'","body":"Bump to mpv-player/mpv@'"$short_sha"'"}'
+  -d '{"tag_name":"'"$date"'","name":"'"$date"'","body":"'"$body"'"}'
   
 release_id=$(curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
   -H "Accept: application/vnd.github.v3+json" \
