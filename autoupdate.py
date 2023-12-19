04ebbb9cb92c6a8b46dd61df50bd2cb3115c435e
eko5624 <562433403@qq.com>
2023-12-19 21:59:25 +0800

    misc

--- autoupdate.py
+++ autoupdate.py
@@ -4,17 +4,6 @@ import in_place
 
 resp = request.urlopen('https://github.com/eko5624/nginx-nosni/raw/master/old.json')
 x = json.loads(resp.read().decode('utf-8'))
-
-mingw = x['Mingw-w64'][:x['Mingw-w64'].find('ucrt')+4]
-
-for t in ['build-toolchain.yml', 'build-toolchain-old.yml']:
-  with in_place.InPlace('.github/workflows/%s' % t, newline='') as f:
-    for l in f:
-      if (i:=l.find('key: mcf_')) > -1:
-        l = '%s%s\n' % (l[:i+9], mingw)
-      elif (i:=l.find('mingw-w64-gcc-mcf_')) > -1:
-        l = '%s%s.7z\n' % (l[:i+18], x['Mingw-w64'])
-      f.write(l)
     
 pkgs = {} 
 pkgs['libsixel'] = x['libsixel']
