a47b58e5c700192c300fc1a36012f4aeab4999cc
eko5624 <562433403@qq.com>
2023-12-20 11:13:13 +0800

    misc

--- release-dev.sh
+++ release-dev.sh
@@ -1,32 +1,31 @@
 #!/bin/bash
 set -x
-CURL=/d/ucrt64/bin/curl
 CURL_RETRIES="--connect-timeout 60 --retry 5 --retry-delay 5 --http1.1"
 
 # Delete assets
-asset_id=$($CURL -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
+asset_id=$(curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
   -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/repos/${GITHUB_REPOSITORY}/releases/tags/dev \
   | jq -r '.assets[] | select(.name | startswith("'"$1"'")) | .id') 
   
-$CURL -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
+curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
   -X DELETE \
   -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/repos/${GITHUB_REPOSITORY}/releases/assets/$asset_id    
 
 # Release assets
-$CURL -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
+curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
   -X POST \
   -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/repos/${GITHUB_REPOSITORY}/releases \
   -d '{"tag_name": "dev"}'
   
-release_id=$($CURL -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
+release_id=$(curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
   -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/repos/${GITHUB_REPOSITORY}/releases/tags/dev | jq -r '.id')
   
 for f in $2/*.zst; do 
-  $CURL -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
+  curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
     -X POST -H "Accept: application/vnd.github.v3+json" \
     -H "Content-Type: $(file -b --mime-type $f)" \
     https://uploads.github.com/repos/${GITHUB_REPOSITORY}/releases/$release_id/assets?name=$(basename $f) --data-binary @$f; 
