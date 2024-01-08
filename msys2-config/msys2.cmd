f0959e4a0fb05e50d4fade78cde7f18b1fcd88a2
eko5624 <562433403@qq.com>
2024-01-08 19:31:53 +0800

    Update msys2.cmd

--- msys2-config/msys2.cmd
+++ msys2-config/msys2.cmd
@@ -1,6 +1,6 @@
 @echo off
 setlocal
 IF NOT DEFINED MSYS2_PATH_TYPE set MSYS2_PATH_TYPE=strict
-IF NOT DEFINED MSYSTEM set MSYSTEM=mingw64
+IF NOT DEFINED MSYSTEM set MSYSTEM=UCRT64
 set CHERE_INVOKING=1
 D:\msys64\usr\bin\bash.exe -leo pipefail %*
