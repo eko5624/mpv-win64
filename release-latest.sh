d49d4d6f09a08b81078e2c49fee9dbe71acc8ad5
eko5624 <562433403@qq.com>
2024-05-17 21:45:11 +0800

    -

--- release-latest.sh
+++ release-latest.sh
@@ -6,7 +6,7 @@ CURL_RETRIES="--connect-timeout 60 --retry 5 --retry-delay 5"
 asset_id=($(curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
   -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/repos/${GITHUB_REPOSITORY}/releases/tags/latest \
-| jq -r '.assets[] | select(.name | startswith("'"$1"'")) | .id' | tr -d '\r'))
+| jq -r '.assets[] | select(.name | contains("'"$1"'")) | .id' | tr -d '\r'))
 
 for id in "${asset_id[@]}"; do
   curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
